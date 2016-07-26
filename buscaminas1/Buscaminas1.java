e5029f9a58bf86dfeb1200cfc7484911dd0b72ff
Josué Muñoz S <jmunozs@ucatolica.ac.cr>
2016-07-25 21:04:26 -0600

    commit main
    Main del busca minas

---
+++ buscaminas1/Buscaminas1.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+//paquetes
+package buscaminas1;
+
+/**
+ **
+ ** @author Josue Muñoz Solis & Daniel Murillo Rodriguez
+ ** @date  
+ **/
+public class Buscaminas1 {
+    /**
+     ** @param args the command line arguments
+     **/
+    public static void main(String[] args) {
+        // TODO code application logic here
+       VentanaPrincipal v = new VentanaPrincipal(); // Instancia de la clase VentanaPrincipal
+        v.setVisible(true); // Metodo para hacer visible el grafico del buscaminas
+    } // Cierre del main
+} // Cierre de la clase buscaminas 
